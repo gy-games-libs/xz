82f26c3d6952218dc8b4c043329f147bcb596ad6
huangzhen <huangzhen@gyyx.cn>
2017-06-02 13:26:24 +0800

    change depend

--- lzma/hashtable.go
+++ lzma/hashtable.go
@@ -8,7 +8,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/ulikunitz/xz/internal/hash"
+	"github.com/gy-games-libs/xz/internal/hash"
 )
 
 /* For compression we need to find byte sequences that match the byte
